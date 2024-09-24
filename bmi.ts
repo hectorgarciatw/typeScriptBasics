01cd2722665ca0701149dca5bef9244ef0d37731
hectorgarciatw <dreamallica@gmail.com>
2024-09-24 11:53:57 -0300

    Exercise 9.1 completed

---
+++ bmi.ts
@@ -0,0 +1,20 @@
+function calculateBmi(height: number, weight: number): string {
+    // Height in cm
+    const heightInMeters = height / 100;
+    const bmi = weight / Math.pow(heightInMeters, 2);
+    let result = "";
+
+    if (bmi < 18.5) {
+        result = "Low (low weight)";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        result = "Normal (healthy weight)";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        result = "High (high weight)";
+    } else {
+        result = "Obesity";
+    }
+
+    return result;
+}
+
+console.log(calculateBmi(180, 74));
